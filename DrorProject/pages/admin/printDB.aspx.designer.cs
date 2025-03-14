70f9f889257e5ab342d1372d0f09f6eab7a7f5ab
dror2009 <drorgilad2009@gmail.com>
2025-03-15 01:40:33 +0200

    visual studio auto change

--- DrorProject/pages/admin/printDB.aspx.designer.cs
+++ DrorProject/pages/admin/printDB.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace DrorProject.pages.admin
 
     public partial class printDB
     {
+
+        /// <summary>
+        /// filter control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm filter;
     }
 }
